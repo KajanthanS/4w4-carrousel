3d5ccd9cf9e68220cb373ba34ed8f3da5a91294e
KajanthanS <e2124480@cmaisonneuve.qc.ca>
2023-04-03 13:05:09 -0400

    s10c1 Début du carrousel

---
+++ carrousel.php
@@ -0,0 +1,9 @@
+<?php
+/** 
+* Plugin name: Carrousel Kajanthan
+* Author: Kajanthan Seevarathinam
+* Author URI: https://github.com/KajanthanS
+* Version: 1.0.0
+* Description: Cette extension carrousel permettra d'afficher dans une boîte modale animée les images d'une galerie.
+*/ 
+?>
